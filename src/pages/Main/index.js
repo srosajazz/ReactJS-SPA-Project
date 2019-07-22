fa8eb154dd821b9c461a04ffa9a1f33e8111e410
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-22 15:53:34 -0400

    Styling Main Page

---
+++ src/pages/Main/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repository
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Add repository" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+
+  svg {
+    margin-right: 10px;
+  }
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 2px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #65bb70;
+  padding-left: 10px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
