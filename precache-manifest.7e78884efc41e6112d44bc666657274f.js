e58e121927d37b74ad19afeaeb5ce76b7e44228c
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-23 20:01:26 -0400

    Updates

---
+++ precache-manifest.7e78884efc41e6112d44bc666657274f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9741c8ccbe723fd7320584bee3d4c0b5",
+    "url": "/ReactJS-SPA-Project/index.html"
+  },
+  {
+    "revision": "05c11970e57496ee2450",
+    "url": "/ReactJS-SPA-Project/static/js/2.914d4865.chunk.js"
+  },
+  {
+    "revision": "d3c915fc7607fdaed18c",
+    "url": "/ReactJS-SPA-Project/static/js/main.3fba4c71.chunk.js"
+  },
+  {
+    "revision": "3c5cb54a6e6d72b4acbe",
+    "url": "/ReactJS-SPA-Project/static/js/runtime~main.9a65b36b.js"
+  }
+]);
